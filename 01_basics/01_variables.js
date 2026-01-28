e98670a393e5dbe01867b72de0d199fd4a92f07b
PrathmeshMule <pmule0804@gmail.com>
2026-01-28 07:26:17 +0000

    Add variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 3405
+let accountEmail = "prathmesh@google.com"
+var accountPassword = "12345"       //old use don't use
+accountCity = "Pune"   // working but don't use
+let accountState = ""
+
+//accountId = 2 / not allowed
+
+accountEmail = "abbcd@e.com"
+accountPassword ="345678"
+accountCity = "Mumbai"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
